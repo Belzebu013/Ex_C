970ffd3629facd6f9eb22be75cd80520e7133fe2
Eduardo Daniel <100101330+Belzebu013@users.noreply.github.com>
2022-10-05 08:05:19 -0300

    Update Ex_4.c

--- Ex_4.c
+++ Ex_4.c
@@ -10,51 +10,38 @@ int main()
  int maior[4][4];
  int i, j;
 
-
  printf("Digite os numeros do vetor:\n");
- for(i=0; i<4; i++)
- {
- for (j = 0; j < 4; j++)
- {
- scanf("%d", &vetorA[i][j]);
- }
+ for(i=0; i<4; i++){
+  for (j = 0; j < 4; j++){
+   scanf("%d", &vetorA[i][j]);
+  }
  }
 
-for (i = 0; i < 4; i++)
- {
- for (j = 0; j < 4; j++)
- {
- if(vetorA[i][j]>10)
- {
- maior[i][j] = vetorA[i][j];
- }
- else
- {
- maior[i][j] = 0;
- }
+ for (i = 0; i < 4; i++){
+  for (j = 0; j < 4; j++){
+   if(vetorA[i][j]>10){
+    maior[i][j] = vetorA[i][j];
+    }else{
+      maior[i][j] = 0;
+   }
 }
  }
 
  printf("\n\n\n");
  printf("Os valores da matriz sao: \n");
- for (i = 0; i < 4; ++i)
- {
- for (j = 0; j < 4; ++j)
- {
- printf("%d\t", vetorA[i][j]);
+ for (i = 0; i < 4; ++i){
+  for (j = 0; j < 4; ++j){
+   printf("%d\t", vetorA[i][j]);
  }
- printf("\n");
+   printf("\n");
  }
 
-printf("\n\n\n");
+ printf("\n\n\n");
  printf("Os valores maiores que 10 sao: \n");
- for (i = 0; i < 4; ++i)
- {
- for (j = 0; j < 4; ++j)
- {
- printf("%d\t", maior[i][j]);
+ for (i = 0; i < 4; ++i){
+  for (j = 0; j < 4; ++j){
+   printf("%d\t", maior[i][j]);
  }
  printf("\n");
  }
-
 }
