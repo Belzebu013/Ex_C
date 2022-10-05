cfe0f85bfaab5293629ffa464333539a230d17ac
Eduardo Daniel <100101330+Belzebu013@users.noreply.github.com>
2022-10-05 08:01:31 -0300

    Update 3.c

--- 3.c
+++ 3.c
@@ -1,4 +1,4 @@
-/*24. Faça um programa que leia dez conjuntos de dois valores, o primeiro representando o número do aluno e o 
+/*Faça um programa que leia dez conjuntos de dois valores, o primeiro representando o número do aluno e o 
 segundo representando a sua altura em metros. Encontre o aluno mais baixo e o mais alto. Mostre o numero do 
 aluno mais baixo e do mais alto, juntamente com suas alturas.*/
 
