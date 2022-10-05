8852e7dbcef16c3c7e7b9cda5022bbd80f514cb8
Eduardo Daniel <100101330+Belzebu013@users.noreply.github.com>
2022-10-04 21:13:02 -0300

    Create Ex_5.C

---
+++ Ex_5.C
@@ -0,0 +1,50 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+const int linhas=3;
+const int colunas=3;
+
+int matriz[3][3];
+int tranposta[3][3];
+int i,j;
+
+int main (){
+	
+//Popular matriz
+	for(i=0;i<linhas;i++) {
+		for(j=0;j<colunas;j++){
+		printf("Digite o numero da posicao [%d][%d] da matriz: \n",i+1,j+1);
+		scanf("%d",&matriz[i][j]);
+		}
+	}
+
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+printf("Matriz Lida: \n");
+
+//imprimir matriz lida
+	for(i=0;i<linhas;i++) {
+		for(j=0;j<colunas;j++){
+			printf("[%d] ",matriz[i][j]);
+		}
+		printf("\n");
+	}
+
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+printf("Matriz Transposta: \n");
+
+//imprimir matriz transposta
+	for(i=0;i<linhas;i++) {
+		for(j=0;j<colunas;j++){
+			printf("[%d] ",matriz[j][i]);
+		}
+		printf("\n");
+	}
+
+} 
